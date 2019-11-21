ad96bf77e207d0dcc474eb7d9dd31d4926a216b1
Richard Rohrkemper <richardrohrkemper@a250xaop.local>
2019-11-21 16:17:29 -0700

    Removed commented out code

--- tic-tac-test.go
+++ tic-tac-test.go
@@ -1,7 +1,6 @@
 package main
 
 import (
-	"fmt"
 	"log"
 	"net/http"
 	s "strings"
@@ -13,29 +12,14 @@ func boardCheck(w http.ResponseWriter, r *http.Request, board *GameBoard) {
 	board.OCount = s.Count(board.Board, "O")
 
 	if board.OCount != (2 * board.Round) {
-		// TODO Create error for wrong number of O's
 		board.isCheating = true
-		fmt.Println("Incorrect number of O's")
-		// TODO: Can I force the browser to a Cheating Page Here?
 	}
 	if board.XCount != (board.Round) {
-		// TODO Create error for wrong number of X's
 		board.isCheating = true
-		fmt.Println("Incorrect number of X's")
-		//r.RequestURI = "/xischeating"
-		// TODO: Can I force the browser to a Cheating Page Here?
-		//t, _ := template.ParseFiles("xischeating.gtpl")
-		//t.Execute(w, board)
 	}
-
-	//board.Round++
 }
 
 func checkPlayerWin(board *GameBoard) {
-
-	// Capture relevant board string
-	//var subBoard = board[:9]
-
 	// Check for player victory
 	if board.Board[0] == 'X' && board.Board[1] == 'X' && board.Board[2] == 'X' {
 		board.playerVictory = true
@@ -60,10 +44,6 @@ func checkPlayerWin(board *GameBoard) {
 }
 
 func checkServerWin(board *GameBoard) {
-
-	// Capture relevant board string
-	//var subBoard = board[:9]
-
 	// Check for server victory
 	if board.Board[0] == 'O' && board.Board[1] == 'O' && board.Board[2] == 'O' {
 		board.serverVictory = true
@@ -85,31 +65,6 @@ func checkServerWin(board *GameBoard) {
 		// base case
 		board.serverVictory = false
 	}
-}
-
-func ticTacTest(w http.ResponseWriter, r *http.Request, board *GameBoard) {
-
-	// Check for valid board
-	boardCheck(w, r, board)
-
-	// Check for player victory
-	checkPlayerWin(board)
-	if board.playerVictory == true {
-		// TODO player victory
-		fmt.Println("Player victory: ", board.playerVictory)
-	}
-
-	// Place server moves
-	board.Board = s.Replace(board.Board, "-", "O", 2)
-
-	// Check for server victory
-	checkServerWin(board)
-	if board.serverVictory == true {
-		// TODO server victory
-		fmt.Println("Server victory: ", board.serverVictory)
-	}
-
-	//board.Round++
 }
 
 type GameBoard struct {
@@ -145,13 +100,10 @@ func game(w http.ResponseWriter, r *http.Request) {
 		b.Board = s.Replace(b.Board, "-", "O", 2)
 		checkServerWin(&b)
 		t.Execute(w, b)
-		//ticTacTest(w, r, &b)
 	case "POST":
 		// Populate board
 		r.ParseForm()
 		b.Board = r.Form.Get("String")
-		//b.XCount = strings.Count(b.Board, "X")
-		//b.OCount = strings.Count(b.Board, "O")
 		// Get cookie value and set for next round
 		cookie, _ := r.Cookie("Round")
 		if cookie.Value == "One" {
@@ -204,40 +156,15 @@ func game(w http.ResponseWriter, r *http.Request) {
 			// do nothing
 		}
 		t.Execute(w, b)
-		//fmt.Println("Beginning Round: ", b.Round)
-		//fmt.Println("Cookie value: ", cookie.Value)
-		//ticTacTest(w, r, &b)
 	default:
 	}
 }
 
 func main() {
-	//b := "---------"
-	//r := 0
-	//reader := bufio.NewReader(os.Stdin)
-	//var playerVictory, serverVictory bool
-
 	http.HandleFunc("/game", game)
 
 	err := http.ListenAndServe(":9090", nil) // setting listening port
 	if err != nil {
 		log.Fatal("ListenAndServe: ", err)
 	}
-
-	//ticTacTest(board)
-	// fmt.Println("Board: ", b2)
-
-	// for playerVictory == false && serverVictory == false {
-	// 	fmt.Println("Board: ", b2)
-	// 	fmt.Print("Enter the next board -> ")
-	// 	text, _ := reader.ReadString('\n')
-	// 	// convert CRLF to LF
-	// 	text = strings.Replace(text, "\n", "", -1)
-	// 	b2 = text
-	// 	b2, r2, playerVictory, serverVictory = ticTacTest(b2, r2)
-	// }
-	// fmt.Println("Round: ", r2)
-	// fmt.Println("Player victory: ", playerVictory)
-	// fmt.Println("Server victory: ", serverVictory)
-
 }
