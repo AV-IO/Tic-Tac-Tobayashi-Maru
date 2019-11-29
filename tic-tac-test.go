c2c85821a861c808a123bccef63f2332a8a418ac
Richard Rohrkemper <richardrohrkemper@a250xaop.local>
2019-11-29 14:54:08 -0700

    Added cookie mapping

--- tic-tac-test.go
+++ tic-tac-test.go
@@ -1,22 +1,72 @@
 package main
 
 import (
+	"crypto/rand"
+	"encoding/base64"
 	"log"
 	"net/http"
 	s "strings"
 	"text/template"
 )
 
+// GenerateRandomBytes returns securely generated random bytes.
+// It will return an error if the system's secure random
+// number generator fails to function correctly, in which
+// case the caller should not continue.
+func GenerateRandomBytes(n int) ([]byte, error) {
+	b := make([]byte, n)
+	_, err := rand.Read(b)
+	// Note that err == nil only if we read len(b) bytes.
+	if err != nil {
+		return nil, err
+	}
+
+	return b, nil
+}
+
+// GenerateRandomStringURLSafe returns a URL-safe, base64 encoded
+// securely generated random string.
+// It will return an error if the system's secure random
+// number generator fails to function correctly, in which
+// case the caller should not continue.
+func GenerateRandomStringURLSafe(n int) (string, error) {
+	b, err := GenerateRandomBytes(n)
+	return base64.URLEncoding.EncodeToString(b), err
+}
+
 func boardCheck(w http.ResponseWriter, r *http.Request, board *GameBoard) {
 	board.XCount = s.Count(board.Board, "X")
 	board.OCount = s.Count(board.Board, "O")
 
+	// Check for expected number of O's
 	if board.OCount != (2 * board.Round) {
 		board.isCheating = true
+
+		// Check for expected number of X's
+	} else if board.XCount != (board.Round) {
+		board.isCheating = true
+
+		// Check for board size >= 9
 	}
-	if board.XCount != (board.Round) {
+	for len(board.Board) < 9 {
+		//board.isCheating = true
+		board.Board += "-"
+		// default case
+	}
+	// Check for any characters other than X, O, and -
+	if restrictedCharacters(board.Board) {
 		board.isCheating = true
 	}
+
+}
+
+func restrictedCharacters(s string) bool {
+	for _, r := range s {
+		if r != 'X' && r != 'O' && r != '-' {
+			return true
+		}
+	}
+	return false
 }
 
 func checkPlayerWin(board *GameBoard) {
@@ -88,10 +138,31 @@ func game(w http.ResponseWriter, r *http.Request) {
 		isCheating:    false,
 	}
 	t, _ := template.ParseFiles("game.gtpl")
+	m := make(map[string]string)
+
+	m["1"], _ = GenerateRandomStringURLSafe(16)
+	m["2"], _ = GenerateRandomStringURLSafe(16)
+	m["3"], _ = GenerateRandomStringURLSafe(16)
+	m["4"], _ = GenerateRandomStringURLSafe(16)
+	m["5"], _ = GenerateRandomStringURLSafe(16)
+	m["6"], _ = GenerateRandomStringURLSafe(16)
+	m["7"], _ = GenerateRandomStringURLSafe(16)
+	m["8"], _ = GenerateRandomStringURLSafe(16)
+	m["9"], _ = GenerateRandomStringURLSafe(16)
+
+	r1 := m["1"]
+	r2 := m["2"]
+	r3 := m["3"]
+	r4 := m["4"]
+	r5 := m["5"]
+	r6 := m["6"]
+	r7 := m["7"]
+	r8 := m["8"]
+	r9 := m["9"]
 
 	switch r.Method {
 	case "GET":
-		http.SetCookie(w, &http.Cookie{Name: "Round", Value: "One"})
+		http.SetCookie(w, &http.Cookie{Name: "Round", Value: r1})
 		b.Board = "---------"
 		b.Round = 0
 		boardCheck(w, r, &b)
@@ -99,24 +170,48 @@ func game(w http.ResponseWriter, r *http.Request) {
 		// Place server moves
 		b.Board = s.Replace(b.Board, "-", "O", 2)
 		checkServerWin(&b)
-		t.Execute(w, b)
 	case "POST":
 		// Populate board
 		r.ParseForm()
 		b.Board = r.Form.Get("String")
 		// Get cookie value and set for next round
 		cookie, _ := r.Cookie("Round")
-		if cookie.Value == "One" {
+		if cookie.Value == r1 {
 			b.Round = 1
-			cookie.Value = "Two"
+			cookie.Value = r2
 			http.SetCookie(w, cookie)
-		} else if cookie.Value == "Two" {
+		} else if cookie.Value == r2 {
 			b.Round = 2
-			cookie.Value = "Three"
+			cookie.Value = r3
 			http.SetCookie(w, cookie)
-		} else if cookie.Value == "Three" {
+		} else if cookie.Value == r3 {
 			b.Round = 3
-			cookie.Value = "Four"
+			cookie.Value = r4
+			http.SetCookie(w, cookie)
+		} else if cookie.Value == r4 {
+			b.Round = 4
+			cookie.Value = r5
+			http.SetCookie(w, cookie)
+		} else if cookie.Value == r5 {
+			b.Round = 5
+			cookie.Value = r6
+			http.SetCookie(w, cookie)
+		} else if cookie.Value == r6 {
+			b.Round = 6
+			cookie.Value = r7
+			http.SetCookie(w, cookie)
+		} else if cookie.Value == r7 {
+			b.Round = 7
+			cookie.Value = r8
+			http.SetCookie(w, cookie)
+		} else if cookie.Value == r8 {
+			b.Round = 8
+			cookie.Value = r9
+			http.SetCookie(w, cookie)
+		} else if cookie.Value == r9 {
+			// Force error to cheaing screen
+			b.Round = 100
+			cookie.Value = r9
 			http.SetCookie(w, cookie)
 		}
 		// Check for proper board
@@ -125,39 +220,26 @@ func game(w http.ResponseWriter, r *http.Request) {
 		// Place server moves
 		b.Board = s.Replace(b.Board, "-", "O", 2)
 		checkServerWin(&b)
-		// Check if the player is cheating
-		switch b.isCheating {
-		case true:
+		switch {
+		case b.isCheating:
 			t, _ = template.ParseFiles("ischeating.gtpl")
-			fallthrough
-		case false:
-			fallthrough
-		default:
-			// do nothing
-		}
-		// Check for server victory
-		switch b.serverVictory {
-		case true:
-			t, _ = template.ParseFiles("servervictory.gtpl")
-			fallthrough
-		case false:
-			fallthrough
-		default:
-			// do nothing
-		}
-		// Check for player victory
-		switch b.playerVictory {
-		case true:
+		case b.playerVictory:
 			t, _ = template.ParseFiles("playervictory.gtpl")
-			fallthrough
-		case false:
-			fallthrough
-		default:
-			// do nothing
+		case b.serverVictory:
+			t, _ = template.ParseFiles("servervictory.gtpl")
 		}
-		t.Execute(w, b)
 	default:
+		t, _ = template.ParseFiles("ischeating.gtpl")
 	}
+	t.Execute(w, b)
+
+	// Cookie Cleanup
+	/*
+		if t.Tree.Name != "game.gtpl" {
+			cookie, _ := r.Cookie("Round")
+			cookie.Value = ""
+			http.SetCookie(w, cookie)
+		}*/
 }
 
 func main() {
